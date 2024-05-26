78865a8fc495ebf2024802cd58dc27bb2e83cfd4
taniacacuango <tania.cacuango329@ist17dejulio.edu.ec>
2024-05-26 00:14:45 -0500

    Main para Instanciar los Objetos

--- src/vista/Main.java
+++ src/vista/Main.java
@@ -4,10 +4,66 @@
  */
 package vista;
 
-/**
- *
- * @author pc1
- */
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import modelo.Cliente;
+import modelo.Descuento;
+import modelo.FormaDePago;
+import modelo.Pedido;
+import modelo.Producto;
+
 public class Main {
-    //nmbdksnnlsfn kjbjonhljknlk
+    public static void main(String[] args) {
+        // Lista de Productos
+        List<Producto> productos = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            productos.add(new Producto(i, "Producto" + i, 10.0 * i, "Categoria" + i, "Talla" + i, "Marca" + i, "Color" + i));
+        }
+
+        // Lista de Clientes
+        List<Cliente> clientes = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            clientes.add(new Cliente(i, "Cliente" + i, "cliente" + i + "@mail.com", 123456 + i, "Direccion" + i));
+        }
+
+        // Lista de Pedidos
+        List<Pedido> pedidos = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            pedidos.add(new Pedido(i, clientes.get(i), new Date(), "Estado" + i, productos, "Direccion" + i, new Date(), 100.0 * i));
+        }
+
+        // Lista de Formas de Pago
+        List<FormaDePago> formasDePago = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            formasDePago.add(new FormaDePago(i, "Metodo" + i, true, "Detalles" + i));
+        }
+
+        // Lista de Descuentos
+        List<Descuento> descuentos = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            descuentos.add(new Descuento(i, 10.0 * i, "Tipo" + i, "Descripcion" + i));
+        }
+
+        // Imprimir todos los objetos
+        for (Producto producto : productos) {
+            producto.imprimir();
+        }
+
+        for (Cliente cliente : clientes) {
+            cliente.imprimir();
+        }
+
+        for (Pedido pedido : pedidos) {
+            pedido.imprimir();
+        }
+
+        for (FormaDePago formaDePago : formasDePago) {
+            formaDePago.imprimir();
+        }
+
+        for (Descuento descuento : descuentos) {
+            descuento.imprimir();
+        }
+    }
 }
